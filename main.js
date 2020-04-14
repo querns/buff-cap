db424e6c8bf12a6cdca3e018c380dede0ce546df
querns <querns@gbs.io>
2020-04-14 16:22:27 -0500

    Adds warrior self buffs, equipment buffs, sharing functionality, about modal

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
+
 (() => {
     const calculateTotalBuffs = () => {
         const badge = $("#total-buff-count");
         let count = 0;
         let badgeClass = "badge-success";
+        let selectedBuffs = [];
 
         $("input[type='checkbox']").each((index, element) => {
             if ($(element).prop("checked")) {
                 count++;
+
+                const id = $(element).prop("id");
+                const match = id.match(/spell-(\d+)/);
+                if (match) {
+                    const spellID = match[1];
+                    selectedBuffs.push(spellID);
+                }
             }
         });
 
@@ -19,10 +28,35 @@
         }
 
         badge.removeClass("badge-success badge-warning badge-danger").addClass(badgeClass).text(count);
+
+        if (selectedBuffs.length > 0) {
+            const bytes16 = new Uint16Array(selectedBuffs.sort());
+            const bytes8 = new Uint8Array(bytes16.buffer);
+            let binary = "";
+            for (let i = 0; i < bytes8.byteLength; i++) {
+                binary += String.fromCharCode( bytes8[i] );
+            }
+            let hash = btoa(binary);
+            hash = hash.replace(/\+/g, '-');
+            hash = hash.replace(/\//g, '_');
+
+            window.location.hash = hash;
+        } else {
+            window.location.hash = "";
+        }
     };
 
+    $("#clear-all").click(() => {
+        $("input[type='checkbox']").prop("checked", false);
+        calculateTotalBuffs();
+        $(".buff-count").text("");
+    });
+
     $(".bc-cell-header button").click(e => {
-        $(e.currentTarget).closest(".bc-cell").find("input[type='checkbox']").prop("checked", false);
+        $(e.currentTarget).closest(".bc-cell")
+            .find("input[type='checkbox']").prop("checked", false)
+            .end()
+            .find(".buff-count").text("");
         calculateTotalBuffs();
     });
 
@@ -55,5 +89,32 @@
     });
 
     $("input[type='checkbox']").click(calculateTotalBuffs);
+
+    const clipboard = new Clipboard("#clipboard-button", {
+        text: () => window.location.href,
+    });
+    clipboard.on("success", () => {
+        $("#copy-toast").toast("show");
+    });
+
+    let hash = window.location.hash;
+    if (hash.length > 0) {
+        hash = hash.replace(/-/g, "+");
+        hash = hash.replace(/_/g, "/");
+        hash = hash.replace(/#/g, "");
+        const raw = atob(hash);
+        const rawLength = raw.length;
+        const int8Array = new Uint8Array(new ArrayBuffer(rawLength));
+
+        for (let i = 0; i < rawLength; i++) {
+            int8Array[i] = raw.charCodeAt(i);
+        }
+
+        const int16Array = new Uint16Array(int8Array.buffer);
+        int16Array.forEach(buff => {
+            $(`#spell-${buff}`).prop("checked", true);
+        });
+    }
+
     calculateTotalBuffs();
 })();
